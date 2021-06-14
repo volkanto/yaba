65ae1be35276c39a22c88a423cf23bcbe832dd35
Volkan Tokmak <volkanto@users.noreply.github.com>
2021-06-14 16:56:38 +0200

    Structure refactoring (#19)

--- bin/index.js
+++ bin/index.js
@@ -2,14 +2,13 @@
 
 // third party lib definitions
 const yargs = require("yargs");
-const pkg = require('../package.json');
 const chalk = require("chalk");
 
 // local variables
-const package = require('../package.json');
 const helper = require('./utils/helper.js');
 const tool = require('./utils/tool.js');
 const flow = require('./utils/flow.js');
+const options = require('./utils/command.js').options;
 
 async function run() {
 
@@ -18,46 +17,6 @@ async function run() {
         // https://www.npmjs.com/package/tiny-updater OR https://www.npmjs.com/package/update-notifier
         // can be used instead below method.
         await tool.checkUpdate(); // check if the yaba cli has newer version
-
-        // yaba commands
-        const options = yargs
-            .usage("Usage: yaba -o <owner> -r <repository> -t <tag> -n <release-name> -b <body> -d <draft> -c")
-            .option("o", {alias: "owner", describe: "The repository owner.", type: "string"})
-            .option("r", {alias: "repo", describe: "The repository name.", type: "string"})
-            .option("t", {alias: "tag", describe: "The name of the tag.", type: "string"})
-            .option("n", {alias: "release-name", describe: "The name of the release.", type: "string"})
-            .option("b", {
-                alias: "body",
-                describe: "Text describing the contents of the tag. If not provided, the default changelog will be generated with the usage of the difference of master and latest release.",
-                type: "string"
-            })
-            .option("d", {
-                alias: "draft",
-                describe: "Creates the release as draft.",
-                type: "boolean"
-            })
-            .option("c", {
-                alias: "changelog",
-                describe: "Shows only changelog without creating the release.",
-                type: "boolean"
-            })
-            .option("i", {
-                alias: "interactive",
-                describe: "Prompt before (draft) release is created (default true)",
-                type: "boolean"
-            })
-            .option("s", {
-                alias: "sound",
-                describe: "Play sound when the release created",
-                type: "boolean"
-            })
-            .epilog(`Copyleft ${new Date().getFullYear()} ${package.author} - ${package.githubProfile}`)
-            .alias('h', 'help')
-            .help('help')
-            .alias('v', 'version')
-            .version(pkg.version)
-            .argv;
-
 
         // check required ENV variables
         flow.checkRequiredEnvVariables();
